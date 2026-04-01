65bf1cf6a9f4a63a7e72c3a2111dc98f7cd26d73
choiym0804 <choiym0804@naver.com>
2026-04-01 10:33:54 +0900

    step-3 View and Model

--- src/main/java/com/example/springstart/HelloController.java
+++ src/main/java/com/example/springstart/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.springstart;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,12 +11,23 @@ public class HelloController {
 
     @GetMapping("/hello")
     @ResponseBody
-    public String hello(){
+    public String hello() {
         return "hello spring";
     }
 
     @GetMapping("/hello-view")
-    public String hello_view(){
+    public String helloView() {
         return "hello";
+    }
+
+    @GetMapping("/hello-mvc")
+    public String helloMvc(@RequestParam(required = false) String name, Model model) {
+
+        if (name == null || name.isBlank()) {
+            name = "guest";
+        }
+
+        model.addAttribute("name", name);
+        return "hello-mvc";
     }
 }
