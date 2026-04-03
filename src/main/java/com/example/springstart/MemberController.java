8c0395ef8e026d3165d5535305367626ae539b35
choiym0804 <choiym0804@naver.com>
2026-04-03 11:56:40 +0900

    step-8 member-controller and PRG

---
+++ src/main/java/com/example/springstart/MemberController.java
@@ -0,0 +1,39 @@
+package com.example.springstart;
+
+import com.example.springstart.domain.Member;
+import com.example.springstart.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService service;
+
+    public MemberController(MemberService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/new")
+    public String joinMember() {
+        return "form";
+    }
+
+    @PostMapping("/new")
+    public String joinMember(@RequestParam("name") String name) {
+        service.join(new Member(0L, name));
+        return "redirect:/member/members";
+    }
+
+    @GetMapping("/members")
+    public String findMembers(Model model) {
+        List<Member> members = service.findMembers();
+        model.addAttribute("members", members);
+        return "members";
+    }
+
+}
